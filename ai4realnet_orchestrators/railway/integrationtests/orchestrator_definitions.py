50befadfc61b6ba230e7364b7db3ac64fa7a3af3
chenkins <chenkins44@gmail.com>
2025-09-22 19:09:11 +0200

    feat(railway-orchestrator): add IDs for all 150 scenario for KPI_NF_045 (EO: Solution Quality).
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- ai4realnet_orchestrators/railway/integrationtests/orchestrator_definitions.py
+++ ai4realnet_orchestrators/railway/integrationtests/orchestrator_definitions.py
@@ -2,6 +2,7 @@
 # Generated with https://github.com/flatland-association/flatland-benchmarks/blob/main/definitions/ai4realnet/gen_ai4realnet_benchmarks_sql.py
 # from https://inesctecpt.sharepoint.com/:x:/r/sites/AI4REALNET/Shared%20Documents/General/WP4%20-%20Validation%20and%20impact%20assessment/Validation%20campaigns/Overview%20tests%20for%20KPI%20on%20validation%20campaign%20hub.xlsx?d=w947339379458465eaaf243a750315375&csf=1&web=1&e=RnrCdf
 from ai4realnet_orchestrators.orchestrator import Orchestrator
+from ai4realnet_orchestrators.railway.test_runner_kpi_nf_045_railway import TestRunner_KPI_NF_045_Railway
 from ai4realnet_orchestrators.railway.test_runner_kpi_pf_026_railway import TestRunner_KPI_PF_026_Railway
 
 railway_orchestrator = Orchestrator(
@@ -14,5 +15,12 @@ railway_orchestrator = Orchestrator(
       ], benchmark_id="3b1bdca6-ed90-4938-bd63-fd657aa7dcd7"
     ),
 
+    # KPI-NF-045: Network Impact Propagation (Railway)
+    "e075d4a7-5cda-4d3c-83ac-69a0db1d74dd": TestRunner_KPI_NF_045_Railway(
+      test_id="e075d4a7-5cda-4d3c-83ac-69a0db1d74dd", scenario_ids=[
+        'bb6302f1-0dc2-43ed-976b-4e5d3126006a', '270d3309-e184-499e-9423-130d1058a960',
+      ],
+      benchmark_id="4b0be731-8371-4e4e-a673-b630187b0bb8"
+    ),
   }
 )
