096984b1541061b8ba602b7a4b345303cd14481f
Manuel Meyer <maenu.meyer@bluewin.ch>
2026-08-04 13:06:05 +0200

    feat: add scenario ids

--- ai4realnet_orchestrators/railway/orchestrator_definitions.py
+++ ai4realnet_orchestrators/railway/orchestrator_definitions.py
@@ -104,7 +104,25 @@ railway_orchestrator = Orchestrator(
     #
     # KPI-DF-016: Delay reduction efficiency (Railway)
     "6ff3c588-357c-41a6-a45a-2bd946b158c8": TestRunner_KPI_DF_016_Railway(
-        test_id="6ff3c588-357c-41a6-a45a-2bd946b158c8", scenario_ids=['ba7f9aac-5e96-4436-bae1-23629c4d153b'], benchmark_id="3b1bdca6-ed90-4938-bd63-fd657aa7dcd7"
+        test_id="6ff3c588-357c-41a6-a45a-2bd946b158c8", scenario_ids=[
+            'bb6302f1-0dc2-43ed-976b-4e5d3126006a', 'ac3d32bf-2694-4405-953b-01849e7923ef', '02e163b8-d8a3-44cb-9fb0-65501dfa35b7', 
+            '7a3ae3eb-b783-44a3-80d4-aa9cb0bd55fb', '01a82553-8d2c-4f84-94df-ccb9f3250734', 'db614cef-8b86-467d-a638-64c25a91ec78', 
+            '3b68eeb2-96f6-4a87-8a2f-5decaf3cb3f0', '7277b987-4cc2-4cb5-a308-bb226c832747', '2b4b92d2-6871-4c20-ad58-11dc51718379', 
+            'e5968696-5497-496b-8fe4-f40a837f7129', '9b603e03-3e2e-4366-8127-96307d3b2ce1', '51081d92-2ff1-40a4-b557-38215c125051', 
+            '5832961b-942f-4d33-8614-c6dd4861ef46', 'e15fa3d0-0da3-4513-a5bd-6082806039a3', '45b93b12-57cc-40ff-b277-82de8ceaec32', 
+            '9ae1a2b5-fe89-4027-b1d8-8c3888862a5e', '7b42a1cc-ce70-4d9a-804f-ac9027a1ee48', 'b30319f8-8953-4433-80b6-5b80c9103bc5', 
+            '25f59eeb-3baf-4668-bdb1-2beb577fbf73', 'a35f5412-b565-4f24-9459-eb9ac1f7fe30', 'f262ffb3-86b9-4db8-8657-f4a96915cb83', 
+            'fec35ca6-d093-4081-8cf1-2f3b8f445bed', '14a20672-ea9c-4205-961a-4f2a3585eea2', '63b933c7-b63c-4ab7-b602-69aa5f91aec9', 
+            '4223ef16-0a01-404d-8024-5a656203d3f8', '59b4935d-cc5d-4792-a395-770bae030d2d', '10ee5c39-9ae9-4e58-bdc8-8a449887574d', 
+            'dc4da56a-bf2d-4351-b280-d418736844de', 'd4b12805-017e-46d9-8fe7-220569a21477', '6acbd77e-18a8-41c9-af81-1ff0ac1a9b0f', 
+            'a7a37c14-b2a7-471c-9ed2-af90ee502d39', '626f428b-0928-48c8-8770-12de6e3b18ed', 'd238a30f-2f16-4e12-83a6-fcf779cd7115', 
+            'ac06a723-e801-470d-991d-b4411368ccfe', '9eb9657e-5c48-461a-a680-7fae151f6800', '1667a5c9-ae17-4628-ba55-74e34cb04332', 
+            '2bb4384c-1119-4756-a2ad-fc2e5c00f952', 'c6ea3dff-9cda-471d-bf8b-a576b17036bb', '03ffcc01-b691-406f-ac53-5d13e9328175', 
+            '119b0c6b-4ce6-4a7f-b49b-ec904577a182', 'f3ee3bb9-3328-450a-98fb-63692042134f', '6c90d7a4-980f-4e13-856c-13117e2edf82', 
+            'f7509133-3083-4454-a963-95302eb66764', 'bf4447a8-9e3e-4b8c-b9ea-4bc5bb009977', 'd271e605-1b68-4609-884c-0e6b2417980a', 
+            '7dd80b01-99c3-4a89-9b4b-f4c878a6d996', 'f9b8c0b3-0968-4324-9b92-35063c49def2', '84db3ada-efe4-4cf7-bf05-3f14bbe2c668', 
+            '05eeb2ea-67fe-405e-b630-43f382dbf246', '90071fa0-a560-4c6e-b2ff-fd59588fbdb7'
+            ], benchmark_id="3b1bdca6-ed90-4938-bd63-fd657aa7dcd7"
     ),
     #
 
